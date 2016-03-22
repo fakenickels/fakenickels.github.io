ff86fa8f7f56a90ca663af8d9ad61212ff227269
Gabriel Rubens <grsabreu@gmail.com>
2016-03-21 21:18:30 -0300

    Prep

--- build.sh
+++ build.sh
@@ -9,12 +9,10 @@ rm -rf ./www/**/*
 # Create new build
 harp compile
 mv www/ ../lasjdflajsdlfjals
-
 echo "\033[1;33m Preparing to deploy\n\033[0m"
-
 git checkout master
 rm -rf ./**/*
-mv www/ ./
+mv ../lasjdflajsdlfjals/**/* ./ -f
 git add --all
 git commit -m "$update"
 echo "\033[1;31mPushing new site to GitHub repo\n\033[0m"
