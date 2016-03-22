8eb4b6fb5e1629e05a70bf053496631103a63b87
Gabriel Rubens <grsabreu@gmail.com>
2016-03-21 21:34:03 -0300

    Add CNAME

--- build.sh
+++ build.sh
@@ -11,8 +11,9 @@ harp compile
 mv www/ ../lasjdflajsdlfjals
 echo "\033[1;33m Preparing to deploy\n\033[0m"
 git checkout master
-rm -rf ./**/*
+g rm -rf ./**/*
 mv ../lasjdflajsdlfjals/**/* ./ -f
+echo "gabrielrubens.com" >> CNAME
 git add --all
 git commit -m "$update"
 echo "\033[1;31mPushing new site to GitHub repo\n\033[0m"
